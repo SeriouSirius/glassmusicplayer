1b416b04c7d8f1666abb988973db9a48339aed1c
SeriouSirius <seriousirius@acma.cc>
2026-06-07 01:21:48 +0800

    對齊側欄數字位置

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -83,7 +83,7 @@ const todayBadge = (() => {
           <path d="M19 3h-1V1h-2v2H8V1H6v2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 16H5V8h14v11zM7 10h5v5H7z"/>
         </svg>
         <span>每日推薦</span>
-        <span class="nav-badge">{{ todayBadge }}</span>
+        <span class="nav-count">{{ todayBadge }}</span>
       </div>
 
       <div class="nav-item" :class="{active: currentView === 'liked'}" @click="navigate('liked')">
